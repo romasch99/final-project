f4f9552405b8e7421a4ecb6982741fb792f47e01
romanas <romanas.chomskis@gmail.com>
2022-02-16 19:18:13 +0200

    implemented client componets

--- client/src/ui/atoms/Block.jsx
+++ client/src/ui/atoms/Block.jsx
@@ -1,1 +1,10 @@
-export const BlockFlex = (<div></div>);
+import styled from "styled-components";
+
+export const Block = styled.div``;
+
+export const BlockFlex = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+`;
